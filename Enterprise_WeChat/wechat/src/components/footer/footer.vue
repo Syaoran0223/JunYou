dde8b2c5c758f2ac304b50635dc4492af8ee2e99
宇直 <516725062@qq.com>
2017-09-11 23:31:17 +0800

    添加替换导航栏css

--- Enterprise_WeChat/wechat/src/components/footer/footer.vue
+++ Enterprise_WeChat/wechat/src/components/footer/footer.vue
@@ -1,8 +1,8 @@
 <template lang="html">
     <div class="v-footer">
-        <router-link to='/'@click='footerShowfunc'>首页</router-link>
-        <router-link to='/'>服务记录</router-link>
-        <router-link to='/'>我的</router-link>
+        <div class="" v-for='(l, index) in footerList':class="{'v-footer-show': index == currentIndex}" @click='currentIndex = index' >
+            <router-link :to='l.to' >{{ l.title }}</router-link>
+        </div>
     </div>
 </template>
 
@@ -10,12 +10,30 @@
 export default {
     data() {
         return {
-            footerShow: true,
+            currentIndex: 2,
+            footerList: [
+                {
+                    to:'/',
+                    title:'首页',
+                    name:'Home',
+                },
+                {
+                    to:'/1',
+                    title:'服务记录',
+                    name:'Home',
+                },
+                {
+                    to:'/',
+                    name:'Home',
+                    title:'我的',
+                },
+            ],
+
         }
     },
     methods: {
-        footerShowfunc() {
-            console.log('this')
+        log() {
+            console.log('index', index)
         }
     },
 }
@@ -33,7 +51,11 @@ export default {
         text-align: center;
         font-size: 20px;
     }
-    .v-footer a{
+    .v-footer div {
+        width: 100%;
+        height: 100%;
+    }
+    .v-footer div a{
         text-decoration: none;
         display: block;
         width: 100%;
